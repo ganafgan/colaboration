e48210f57bda356a2f76bba5ff92e840dc6a78c0
fikri <me@fikri.tech>
2020-04-24 13:52:57 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
+      <h3>ini h34</h3>
     </div>
   );
 }
